45049051828beeb291447728b2993c970e104496
eduardolalo9 <eduardolalo9606@gmail.com>
2026-04-04 18:22:16 -0600

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -11,12 +11,13 @@
  */
 
 const FIREBASE_CONFIG = {
-    apiKey:            "AIzaSyDugu23uEgacqMUTsoBF8i7xfyDIDbiv0M",
-    authDomain:        "bar-inventario-1109e.firebaseapp.com",
-    projectId:         "bar-inventario-1109e",
-    storageBucket:     "bar-inventario-1109e.firebasestorage.app",
-    messagingSenderId: "450765028668",
-    appId:             "1:450765028668:web:54fdb19714d374ff02b239"
+     apiKey: "AIzaSyDugu23uEgacqMUTsoBF8i7xfyDIDbiv0M",
+  authDomain: "bar-inventario-1109e.firebaseapp.com",
+  databaseURL: "https://bar-inventario-1109e-default-rtdb.firebaseio.com",
+  projectId: "bar-inventario-1109e",
+  storageBucket: "bar-inventario-1109e.firebasestorage.app",
+  messagingSenderId: "450765028668",
+  appId: "1:450765028668:web:54fdb19714d374ff02b239"
 };
 
 window.FIRESTORE_DOC_ID = "barra-principal";
